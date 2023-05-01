3ca5e5236db85b4863dd6ba166dd5336c8973726
ageofrage <agehara.dev@gmail.com>
2023-05-01 23:09:01 +0900

    :sparkles: read file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let query = &args[1];
     let filename = &args[2];
-    println!("searching for {}", query);
+    println!("Searching for {}", query);
     println!("In file {}", filename);
+
+    let mut file = File::open(filename).expect("file not found");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+    println!("With text:\n{}", contents);
 }
