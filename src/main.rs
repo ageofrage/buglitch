2f9ba8caf58b1d265bc4c16d4cc54c0d814d7943
ageofrage <agehara.dev@gmail.com>
2023-05-01 23:57:36 +0900

    :recycle: split fn

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,8 @@ use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let (query, filename) = parse_config(&args);
 
-    let query = &args[1];
-    let filename = &args[2];
     println!("Searching for {}", query);
     println!("In file {}", filename);
 
@@ -15,4 +14,10 @@ fn main() {
     file.read_to_string(&mut contents)
         .expect("something went wrong reading the file");
     println!("With text:\n{}", contents);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+    (query, filename)
 }
